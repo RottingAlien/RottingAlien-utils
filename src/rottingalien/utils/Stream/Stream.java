12a6966e7327e145e9282bde41483900a844d456
Alex <cradleofcorpses@gmail.com>
2019-06-20 16:06:54 +0100

    adjust stream section and added Bytereader

--- src/rottingalien/utils/Stream/Stream.java
+++ src/rottingalien/utils/Stream/Stream.java
@@ -5,57 +5,17 @@ import java.io.*;
 public class Stream {
 
     /**
-     * Closes FileReader Stream
+     * Closes Stream
      *
-     * @param fileReader FileReader Stream to close.
+     * @param stream Stream to close.
      */
-    public static void closeFileReader(FileReader fileReader) {
-        try {
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Closes FileWriter Stream
-     *
-     * @param fileWriter FileWriter Stream to close.
-     */
-    public static void closeFileWriter(FileWriter fileWriter) {
-        try {
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Closes FileWriter Stream
-     *
-     * @param inputStream FileWriter Stream to close.
-     */
-    public static void closeInPutStream(InputStream inputStream) {
-        try {
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Closes FileWriter Stream
-     *
-     * @param outputStream FileWriter Stream to close.
-     */
-    public static void closeOutPutStream(OutputStream outputStream) {
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void close(Closeable stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
